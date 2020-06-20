67eded7b0916c0f8063e0102767f43d1f64b583d
tallkeith <keithpnash@gmail.com>
2020-06-20 10:49:36 -0400

    Add Comparison class for comparing files

---
+++ diff_detector.rb
@@ -0,0 +1,20 @@
+class Comparison
+  def initialize(file1, file2)
+    @file1 = file1
+    @file2 = file2
+  end
+
+  def self.print_difference(file1, file2)
+    puts new(file1, file2).difference
+  end
+
+  def difference
+    read_file(@file1).delete_if { |line| read_file(@file2).include? line }
+  end
+
+  def read_file(file)
+    File.readlines(file).map(&:chomp)
+  end
+end
+
+Comparison.print_difference('file1.txt', 'file2.txt')
