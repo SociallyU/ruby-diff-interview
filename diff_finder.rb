0513fc6a1b1b91eebfda42f7efbbf44564cba02a
github username <github email address>
2020-06-24 23:06:49 -0700

    methods added for sorted and unsorted files

--- diff_finder.rb
+++ diff_finder.rb
@@ -1,15 +1,14 @@
 class DiffFinder
 
 
-    ## Finds diffs between two files
-    def self.find_diff(txt1, txt2)
+    ## Finds diffs between two files in case of an unsorted file. 
+    ## O(N^2) because txt2 must be iterated through each atttempt
+    def self.find_unsorted_diffs(txt1, txt2, new_file_name)
         diff_arr = []
-
-
-
+        File.foreach(txt1) { |line| !File.read(txt2).include?(line) ? diff_arr.push(line) : nil }
+        File.write(new_file_name, diff_arr.join(""))
     end
 
-    ## Determines 
-    
-
 end
+
+DiffFinder.find_unsorted_diffs("file1.txt", "file2.txt", "new_file.txt")
