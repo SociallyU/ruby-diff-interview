4cb76c7604532c6135e00ef9cd1f22c826ea3e59
github username <github email address>
2020-06-25 01:00:40 -0700

    Fixed iterative logic, added sorting method, refactored to be DRYer

--- diff_finder.rb
+++ diff_finder.rb
@@ -21,7 +21,34 @@ class DiffFinder
     ##
     ## NOTE: the example texts given are not sorted. Therefore, this method
     ## will not work on those files. 
+
+    def self.find_sorted_diffs(txt1, txt2, new_file_name)
+        first_file_array = File.read(txt1).split("\n")
+        second_file_array = File.read(txt2).split("\n")
+        diffs = self.compare_sorted_arrays(first_file_array, second_file_array)
+        File.write(new_file_name, diffs)
+    end
+
+    ## Finds diffs between two files that are unsorted BY sorting
+    ## Between O(N log N) and O(N^2) - Ruby defaults to using quicksort,
+    ## which is O(N log N) in most cases, then the 
+    ## algorithm must iterate through each array once more
+    ## 
+    ## This method differs from self.find_sorted_diffs only in that
+    ## it first sorts the file arrays. It is a moderate time complexity
+    ## improvement over self.find_unsorted_diffs
     ##
+
+    def self.find_unsorted_diffs_with_sort(txt1, txt2, new_file_name)
+        first_file_array = File.read(txt1).split("\n").sort
+        second_file_array = File.read(txt2).split("\n").sort
+        diffs = self.compare_sorted_arrays(first_file_array, second_file_array)
+        File.write(new_file_name, diffs)
+    end
+
+    ## This method compares two sorted arrays to find their diffs.
+    ## Close to O(N) because each file must be iterated through once
+    ## 
     ## Several variables are declared to start. A diff_arr to hold the
     ## lines contained in the first file but not in the second, two arrays
     ## to hold the contents of the files, and two iterators to keep
@@ -33,26 +60,25 @@ class DiffFinder
     ## we continue to advance location within the second file until we find
     ## a value equal to or greater than the current line in the first file.
 
-    def self.find_sorted_diffs(txt1, txt2, new_file_name)
+    def self.compare_sorted_arrays(first_file_array, second_file_array)
         diff_arr = []
-        first_file_array = File.read(txt1).split("\n")
-        second_file_array = File.read(txt2).split("\n")
         i = 0
         j = 0
-
         while i < first_file_array.length
-            if first_file_array[i] == second_file_array[j]
+            if second_file_array[j] == nil
+                diff_arr.push(first_file_array.slice(i, first_file_array.length-i))
+                i = first_file_array.length
+            elsif first_file_array[i] == second_file_array[j]
                 i += 1
                 j += 1
-            elsif first_file_array[i] > second_file_array[j]
+            elsif first_file_array[i] < second_file_array[j]
                 diff_arr.push(first_file_array[i])
                 i += 1
             else
                 j += 1
             end
         end
-
-        File.write(new_file_name, diff_arr.join("\n"))
+        return diff_arr.join("\n")
     end
 
 end
